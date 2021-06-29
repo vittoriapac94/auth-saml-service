c7f5bc2f2b53e7d25d74dba73962feaa18205a19
Vittoria <vittoriapacchiano@yahoo.it>
2021-06-29 21:45:36 +0200

    Commit

--- src/main/java/com/example/authsamlservice/controllers/IndexController.java
+++ src/main/java/com/example/authsamlservice/controllers/IndexController.java
@@ -5,24 +5,37 @@ import java.util.List;
 import com.example.authsamlservice.domains.User;
 import com.google.gson.Gson;
 import org.apache.commons.lang.StringUtils;
+import org.opensaml.common.SAMLException;
+import org.opensaml.saml2.metadata.provider.MetadataProviderException;
+import org.opensaml.ws.message.encoder.MessageEncodingException;
+import org.opensaml.xml.security.SecurityException;
+import org.opensaml.xml.validation.ValidationException;
+import org.springframework.security.saml.context.SAMLMessageContext;
 import org.opensaml.saml2.core.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.providers.ExpiringUsernameAuthenticationToken;
 import org.springframework.security.saml.SAMLCredential;
+import org.springframework.security.saml.metadata.MetadataManager;
+import org.springframework.security.saml.websso.SingleLogoutProfileImpl;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.security.core.Authentication;
 
 @RestController
 public class IndexController {
     String email = "";
 
-    @RequestMapping("/logout")
-    public ModelAndView logout(ExpiringUsernameAuthenticationToken userToken) {
+    /*@RequestMapping("/logout")
+    public ModelAndView logout(ExpiringUsernameAuthenticationToken userToken) throws MessageEncodingException, SAMLException, MetadataProviderException, ValidationException, SecurityException {
         //ModelAndView modelAndView = new ModelAndView("redirect:https://dev-56778466.okta.com/app/dev-56778466_springsaml_1/exkzgi53nhiiaLeBr5d6/slo/saml");
-        ModelAndView modelAndView = new ModelAndView("redirect:/");
+        ModelAndView modelAndView = new ModelAndView("/logout");
+        //SingleLogoutProfileImpl slo = new SingleLogoutProfileImpl();
+        //slo.sendLogoutRequest((SAMLMessageContext) SecurityContextHolder.getContext(), (SAMLCredential) userToken.getCredentials());
+
         return modelAndView;
-    }
+    }*/
 
     @RequestMapping("/")
     public ModelAndView index(ExpiringUsernameAuthenticationToken userToken, Model model) {
@@ -30,6 +43,13 @@ public class IndexController {
         System.out.println("*******************************************");
 
         SAMLCredential credential = (SAMLCredential) userToken.getCredentials();
+
+        Authentication authentication = (Authentication) userToken;
+        System.out.println("PROVA: "+authentication.getName());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        System.out.println("auth: "+auth);
+
         List<Attribute>  attributes = credential.getAttributes();
         System.out.println("userToken.getName()" + userToken.getName());
         String firstName = "";
